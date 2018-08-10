75a14ab291edbfdd4cfda8dec2fba167b8a12808
Pierre Mavro <pmavro@4tech.io>
2018-08-10 03:27:15 +0200

    refactor: run.sh hide secret info at start

--- run.sh
+++ run.sh
@@ -5,7 +5,9 @@ echo "Starting Kubernetes DNS Updater"
 while IFS='=' read -r name value ; do
   if [[ $name == 'KDU_'* ]]; then
     val="${!name}"
-    echo "$name $val"
+    if [ "$name" != "KDU_Key" ] && [ "$name" != "KDU_Email" ] ; then
+      echo "$name $val"
+    fi
     field=$(echo $name | sed -r 's/KDU_(.+)/\1/')
     sed -ri "s/($field):.*/\1: $val/" "/etc/k8s-dns-updater/config.yaml"
   fi
